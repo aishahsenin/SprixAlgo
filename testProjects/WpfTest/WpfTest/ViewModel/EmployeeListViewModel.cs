b51e049af3e92a0ceda210104e394ee4e3b1f63f
aishahsenin <aishahsenin@gmail.com>
2015-11-03 18:12:51 +0000

    adding a new view model called employee list

---
+++ testProjects/WpfTest/WpfTest/ViewModel/EmployeeListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using WpfTest.DataAccess;
+
+namespace WpfTest.ViewModel
+{
+    class EmployeeListViewModel : ViewModelBase
+    {
+
+        // need an instance of employee repository
+        readonly EmployeeRepository _employeeRepository;
+
+        // enable to repo to be consumed by the view
+        public ObservableCollection<Model.Employee> AllEmployees
+        {
+            get;
+            private set; // don't want to be set from the outside
+        }
+
+        // Constructor
+        public EmployeeListViewModel(EmployeeRepository employeeRepository)
+        {
+            if (employeeRepository == null)
+            {
+                throw new ArgumentNullException("Employee repository is empty");
+            }
+
+            _employeeRepository = employeeRepository;
+            this.AllEmployees = new ObservableCollection<Model.Employee>(_employeeRepository.GetEmployees());
+        }
+
+        // override OnDispose method from ViewModelBase...
+        protected override void OnDispose()
+        {
+            this.AllEmployees.Clear();
+        }
+    }
+}
