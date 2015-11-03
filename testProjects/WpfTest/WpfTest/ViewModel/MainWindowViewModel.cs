c0d8e485ccadde065ff139a39d80d46e01911b16
aishahsenin <aishahsenin@gmail.com>
2015-11-03 18:13:40 +0000

    adding a view model for the main window

--- testProjects/WpfTest/WpfTest/ViewModel/MainWindowViewModel.cs
+++ testProjects/WpfTest/WpfTest/ViewModel/MainWindowViewModel.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using WpfTest.DataAccess;
 
 namespace WpfTest.ViewModel
 {
-    class MainWindowViewModel
+    // inheriting from ViewModelBase class
+    public class MainWindowViewModel : ViewModelBase()
     {
+
+        // reference from the employee repository
+        readonly EmployeeRepository _employeeRepository;
+
+        // view model collection, using the view model to display the view
+        ObservableCollection<ViewModelBase> _viewModels;
+
+        public MainWindowViewModel()
+        {
+            _employeeRepository = new EmployeeRepository();
+            
+            // TODO; create an instance of our view model to display, then add to our collection
+        }
+
+        // ViewModels, what we used to reference on the XAML code on MainWindow.xaml
+        // property of the ViewModel that will be accessible from the View
+        public ObservableCollection<ViewModelBase> ViewModels
+        {
+            get
+            {
+                if (_viewModels == null)
+                {
+                    _viewModels = new ObservableCollection<ViewModelBase>();
+                }
+
+                return _viewModels;
+            }
+        }
+
     }
 }
