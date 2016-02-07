094a3e3c1d2f2b860af20ebfd8e10d43ae9a6332
Aishah Senin <aishahsenin@gmail.com>
2016-02-07 13:00:15 +0000

    added database using Azure. Need to design DB

--- Source/SprixProject/SprixProject/Controllers/HomeController.cs
+++ Source/SprixProject/SprixProject/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SprixProject.Models;
 
 namespace SprixProject.Controllers
 {
@@ -26,6 +27,34 @@ namespace SprixProject.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult AnimationStart(int id)
+        {
+            // List of paradigm types
+            //var paradigm = new List<ParadigmType>
+            //{
+            //    new ParadigmType { ID = 1, Name = "Greedy method", Description = "Description about greedy method " },
+            //    new ParadigmType { ID = 2, Name = "Divide and Conquer", Description = "Description about divide and conquer" },
+            //    new ParadigmType { ID = 3, Name = "Dynamic Programming", Description = "Description about dynamic programming" }
+            //};
+
+            //// TODO; 
+            //var algorithm = new List<Algorithm>
+            //{
+            //    new Algorithm { ID = 1, Title = "Fractional Knapsack Problem", Type = { ID = 1, Name = "Greed", Description = "some desc" } },
+            //    new Algorithm { ID = 2, Title = "Activity Scheduling", Type = { ID = 2, Name = "Greed2", Description = "some desc2" } }
+
+            //};
+
+            var algorithm = new Algorithm()
+            {
+                ID = 1,
+                Title = "something",
+                Type = { ID = 1, Name = "greedy", Description = "something desc" }
+            };
+
+            return View(algorithm);
         }
 
 
