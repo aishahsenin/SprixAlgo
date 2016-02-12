ebd8e9b0e01753a39440d472702d9fbc4696fa3d
Aishah Senin <aishahsenin@gmail.com>
2016-02-12 20:19:38 +0000

    script jquery for retrieving noOfIndex val

---
+++ Source/SprixProject/SprixProject/Scripts/jquery_form.js
@@ -0,0 +1,18 @@
+﻿// INFO: Returns the number of index needed to produce a form for index input
+function GetNoOfIndexVal(btnClicked) {
+    var $form = $(btnClicked).parents('form');
+
+    $.ajax({
+        type: "POST",
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        error: function (xhr, status, error) {
+            //do something about the error
+        },
+        success: function (response) {
+            console.log(document.getElementById('NoOfIndexVal').value);
+
+        }
+    });
+    return false;
+}
