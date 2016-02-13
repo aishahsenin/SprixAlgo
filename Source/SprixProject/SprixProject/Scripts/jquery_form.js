a3eea3907e63ac135aec4955b8f3f246962531f5
Aishah Senin <aishahsenin@gmail.com>
2016-02-13 19:30:55 +0000

    sort form, using incerement form instead

--- Source/SprixProject/SprixProject/Scripts/jquery_form.js
+++ Source/SprixProject/SprixProject/Scripts/jquery_form.js
@@ -1,4 +1,6 @@
-﻿// INFO: Returns the number of index needed to produce a form for index input
+﻿var variableInputCount = 2;
+
+// INFO: Returns the number of index needed to produce a form for index input
 function GetNoOfIndexVal(btnClicked) {
     var $form = $(btnClicked).parents('form');
 
@@ -10,17 +12,21 @@ function GetNoOfIndexVal(btnClicked) {
         },
         success: function (response) {
             var limit = document.getElementById("NoOfIndexVal").value;
-            for (i = 0; i < limit; i++) {
-                var indexValueName = "indexValue_" + i;
-                //var textBox = '&nbsp;&nbsp;<input type="textbox" name="' + indexValueName + '" />';
-                var textBox = '@Html.TextBoxFor(m => m.form.IndexValue, htmlAttributes: new { id = "' + indexValueName + '", @class = "form-control" })';
-
-                
-
-                $('div#indexValues').append(textBox);
-            }
-            //document.getElementById("NoOfIndexVal").innerHTML = s;
+            var indexValueName = "indexValue_" + i;
+            //var textBox = '<div class="form-group"><div class="col-md-2 control-label">Index ' + i + '</div><div id="indexValues"class="col-md-4"><input type="textbox" name="' + indexValueName + '" class="form-control"></div></div>';
+            var textBox = '<div class="form-group"><div class="col-md-2 control-label">Index no. ' + count + ' value</div><div class="col-md-4"><div class="input-group"><input type="textbox" name="' + indexValueName + '" class="form-control"><span class="input-group-btn"><button class="btn btn-default" type="button" onclick="GetNoOfIndexVal(this)">Add</button></span></div></div></div>'
+            $('div#here').append(textBox);
         }
     });
     return false;
+}
+
+function AddOneMoreTextBox(btnClicked) {
+    console.log('count = ' + variableInputCount);
+    var indexValueName = "indexValue_" + variableInputCount;
+    var textBox = '<div class="form-group"><div class="col-md-5 control-label">Index no. ' + variableInputCount + ' value</div><div class="col-md-3"><input type="textbox" name="' + indexValueName + '" class="form-control"></div></div>'
+    $('div#here').append(textBox);
+
+    variableInputCount += 1;
+
 }
