b066cb58024590c0c4cb9d7a8cb257949682ac68
Aishah Senin <aishahsenin@gmail.com>
2016-02-27 18:30:28 +0000

    old code, new strategy, redoing functions

--- Source/SprixProject/SprixProject/Scripts/bubbleSortAnimation.js
+++ Source/SprixProject/SprixProject/Scripts/bubbleSortAnimation.js
@@ -24,34 +24,82 @@ function loopThrough() {
     // loop through indexes
     var noOfTotalIndex = document.getElementById('hidNoOfTotalIndex').getAttribute('value');
 
+    // creating a deferred object
+    //var deff = $.Deferred();
+    //deff.resolve();
+
     for (i = 0; i < 3; i++) {
 
-        console.log("0 - i = " + i);
+        // this function loops through the index
+        function loopThis() {
 
-        if (i != (3 - 1)) {
+            console.log("0 - i = " + i);
 
-            var valA = document.getElementById('index' + (i + 1)).innerHTML;
-            var valB = document.getElementById('index' + (i + 2)).innerHTML;
+            // ensure that loop doesn't go outside its range
+            if (i != (3 - 1)) {
 
-            console.log("1 - comparing ValA and ValB " + valA + " " + valB);
+                var valA = document.getElementById('index' + (i + 1)).innerHTML;
+                var valB = document.getElementById('index' + (i + 2)).innerHTML;
 
-            if (valA > valB) {
+                console.log("1 - comparing ValA and ValB " + valA + " " + valB);
 
-                console.log("2 (triggered by if) - swapping ValA and ValB... ");
+                if (valA > valB) {
 
-                var a = i + 1;
-                var b = i + 2;
+                    console.log("2 (triggered by if) - swapping ValA and ValB... ");
 
-                // swap
-                swap(a, b);
+                    var a = i + 1;
+                    var b = i + 2;
 
+                    //var r = $.Deferred();
+
+                    // this function swaps valA and ValB
+                    function swap() {
+
+                        console.log("3 - swapping these vals a = " + a + " b = " + b);
+
+                        var elem1 = document.getElementById("index" + a); // if this becomes dynamic, the animation works all at once
+                        var elem2 = document.getElementById("index" + b);
+                        var pos1 = 0;
+                        var pos2 = 0;
+
+                        console.log("4 - ..:: FRAME RUNNING ::..");
+                        var keepRunning = setInterval(frame, 5);
+
+                        function frame() {
+                            if (pos1 == -55) {
+                                clearInterval(keepRunning);
+                                console.log("5 - ====== FRAME STOPPED ======"); // NOTE: frame needs to stop before proceeding!!!!
+                                sleep(2000);
+                                console.log("6 - Sleeping ... ");
+                                r.resolve();
+                            }
+                            else {
+                                pos1--;
+                                pos2--;
+                                elem1.style.bottom = pos1 + 'px';
+                                elem2.style.bottom = pos2 + 'px';
+                                //console.log("pos1 = " + pos1);
+                                //console.log("pos2 = " + pos2);  
+                            }
+                        }
+
+                    }
+
+
+                }
             }
         }
+
+        console.log("deffered");
+        //deff.done(swap, loopThis);
     }
+
+    return r;
 }
 
+/*
 function swap(a, b) {
-    
+
     console.log("3 - swapping these vals a = " + a + " b = " + b);
 
     var elem1 = document.getElementById("index" + a); // if this becomes dynamic, the animation works all at once
@@ -80,3 +128,4 @@ function swap(a, b) {
     }
 
 }
+*/
